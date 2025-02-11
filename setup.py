f7864f5dc62fc7ed1ba7ca7a7a5209605564cf19
5hojib <yesiamshojib@gmail.com>
2025-02-11 15:27:11 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension, find_packages
 
 setup(
     name="ElectroCrypto",
-    version="1.0.2dev10",
+    version="2.0.0",
     description="Fast and Portable Cryptography Extension Library for Electrogram",
     long_description="Hello",
     long_description_content_type="text/markdown",
